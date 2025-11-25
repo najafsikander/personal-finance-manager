66e9725eeb10d30878048ef1056ab358d9b9079f
najafsikander <najafsikander@yahoo.com>
2025-11-25 17:31:44 +0400

    fixed some file structures issues

---
+++ src/components/pages/Dashboard/SummaryCards.tsx
@@ -0,0 +1,41 @@
+import SummaryCard from "@/components/cards/SummaryCard";
+
+const SummaryCards = () => {
+  return (
+    <>
+      <section className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
+        {/* Example Card */}
+        <SummaryCard
+          header="Total Income"
+          amount="$5000"
+          percentage="35%"
+          subtext="Increased from last month"
+          bgColor="bg-purple-500"
+        />
+        <SummaryCard
+          header="Total Spending"
+          amount="$3500"
+          percentage="75%"
+          subtext="Increased from last month"
+          bgColor="bg-orange-400"
+        />
+        <SummaryCard
+          header="Spending Goal"
+          amount="$9254"
+          percentage="15%"
+          subtext="Increased from last month"
+          bgColor="bg-blue-400"
+        />
+        <SummaryCard
+          header="Total Transaction"
+          amount="$17000"
+          percentage="85%"
+          subtext="Increased from last month"
+          bgColor="bg-green-400"
+        />
+      </section>
+    </>
+  );
+};
+
+export default SummaryCards;
--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import SummaryCard from "@/components/SummaryCard";
+import SummaryCards from "@/components/pages/Dashboard/SummaryCards";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/")({
@@ -10,13 +10,7 @@ function RouteComponent() {
     <>
       <h1 className="text-4xl font-medium text-white text-center">Dashboard</h1>
       {/* Summary Cards Grid */}
-      <section className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-        {/* Example Card */}
-        <SummaryCard header="Total Income" amount="$5000" percentage="35%" subtext="Increased from last month" bgColor="bg-purple-500" />
-        <SummaryCard header="Total Spending" amount="$3500" percentage="75%" subtext="Increased from last month" bgColor="bg-orange-400" />
-        <SummaryCard header="Spending Goal" amount="$9254" percentage="15%" subtext="Increased from last month" bgColor="bg-blue-400" />
-        <SummaryCard header="Total Transaction" amount="$17000" percentage="85%" subtext="Increased from last month" bgColor="bg-green-400" />
-      </section>
+      <SummaryCards/>
     </>
   );
 }
