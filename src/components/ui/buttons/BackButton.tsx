0b9568ebaea42f51480b45cff2f841da3f9d3880
najafsikander <najafsikander@yahoo.com>
2026-03-10 12:51:23 +0400

    Made upper row of add form

---
+++ src/components/ui/buttons/BackButton.tsx
@@ -0,0 +1,9 @@
+const BackButton = () => {
+  return (
+    <button className="border border-white rounded-md py-2 px-4 bg-transparent text-white hover:bg-white hover:cursor-pointer hover:text-slate-800 transition-colors duration-10">
+      BACK
+    </button>
+  );
+};
+
+export default BackButton;
--- src/routes/expenditures/add/index.tsx
+++ src/routes/expenditures/add/index.tsx
@@ -1,10 +1,23 @@
-import ExpenseForm from "@/components/pages/Expenditures/Add/ExpenseForm";
+import BackButton from "@/components/ui/buttons/BackButton";
 import { createFileRoute } from "@tanstack/react-router";
+import { FilePlus } from "lucide-react";
 
 export const Route = createFileRoute("/expenditures/add/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <ExpenseForm />;
+  return (
+    <>
+      <section className="w-full text-center border border-stone-50">
+        <div className="w-full flex justify-between items-center p-2 mb-2">
+          <div className="flex justify-start items-baseline gap-2">
+            <FilePlus size={22} color="white" />
+            <h3 className="font-medium text-xl text-white mb-2">Add Expense</h3>
+          </div>
+          <BackButton />
+        </div>
+      </section>
+    </>
+  );
 }
