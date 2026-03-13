e949a9551f2c717a7046e539a8cdf4c4cf4904a1
najafsikander <najafsikander@yahoo.com>
2026-03-13 15:04:21 +0400

    Added base layout to bulk upload page and made reusable back button working

--- src/components/ui/buttons/BackButton.tsx
+++ src/components/ui/buttons/BackButton.tsx
@@ -1,6 +1,16 @@
+import { useRouter } from "@tanstack/react-router";
+
 const BackButton = () => {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.history.back();
+  };
   return (
-    <button className="border border-white rounded-md py-2 px-4 bg-transparent text-white hover:bg-white hover:cursor-pointer hover:text-slate-800 transition-colors duration-10">
+    <button
+      onClick={handleBack}
+      className="border border-white rounded-md py-2 px-4 bg-transparent text-white hover:bg-white hover:cursor-pointer hover:text-slate-800 transition-colors duration-10"
+    >
       BACK
     </button>
   );
--- src/routes/expenditures/bulk-upload/index.tsx
+++ src/routes/expenditures/bulk-upload/index.tsx
@@ -1,9 +1,51 @@
+import PageArea from "@/components/layout/PageArea";
 import { createFileRoute } from "@tanstack/react-router";
+import { FilePlus } from "lucide-react";
 
 export const Route = createFileRoute("/expenditures/bulk-upload/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/expenditures/bulk-upload/"!</div>;
+  const icon = <FilePlus size={22} color="white" />;
+  const mainAreaDescription = (
+    <>
+      <p>
+        Record a new expense by filling out the form on the right. Provide the
+        payment name, amount, date, payment method, and category to accurately
+        track your transactions.
+      </p>
+
+      <p>
+        Once submitted, the expense will be saved to your financial records.
+      </p>
+
+      <p>
+        If you have multiple transactions, you can also{" "}
+        <a
+          href="/expenditures/bulk-upload"
+          className="text-blue-600 font-medium hover:underline"
+        >
+          upload them using a file
+        </a>
+        .
+      </p>
+    </>
+  );
+
+  return (
+    <section className="w-full flex justify-center">
+      <PageArea
+        icon={icon}
+        upperRowHeading="Bulk Expense Upload"
+        mainAreaHeading="Bulk Upload Info"
+        mainAreaDescription={mainAreaDescription}
+        mainContent={
+          <>
+            <p>Add expenses using a file</p>
+          </>
+        }
+      />
+    </section>
+  );
 }
