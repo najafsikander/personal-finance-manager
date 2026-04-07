c56082ba4a204018f8dd8f003aebe00fb0477081
najafsikander <najafsikander@yahoo.com>
2026-04-07 12:24:29 +0400

    Added bottom pagination row without styling

--- src/components/pages/Dashboard/TransactionTable.tsx
+++ src/components/pages/Dashboard/TransactionTable.tsx
@@ -3,6 +3,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { FC, useState } from "react";
@@ -14,11 +15,18 @@ type Props = {
 
 const TransactionTable: FC<Props> = ({ transactions, columns }) => {
   const [data, _setData] = useState(() => [...transactions]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0, //initial page index
+    pageSize: 2, //default page size
+  });
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    state: { pagination },
   });
   return (
     <div className="p-4">
@@ -63,6 +71,44 @@ const TransactionTable: FC<Props> = ({ transactions, columns }) => {
             ))}
           </tbody>
         </table>
+        <div>
+          <button
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<<"}
+          </button>
+          <button
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<"}
+          </button>
+          <button
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {">"}
+          </button>
+          <button
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {">>"}
+          </button>
+          <select
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </div>
   );
