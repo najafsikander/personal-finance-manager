c95d8044ea29792c92daf38a1c03ba13e7e39d65
najafsikander <najafsikander@yahoo.com>
2026-04-27 12:23:58 +0400

    made first row of profile

--- src/routes/settings/profile/index.tsx
+++ src/routes/settings/profile/index.tsx
@@ -1,6 +1,5 @@
 import Profile from "@/components/ui/avatar/profile";
 import { createFileRoute } from "@tanstack/react-router";
-import { Image } from "@unpic/react";
 
 export const Route = createFileRoute("/settings/profile/")({
   component: RouteComponent,
@@ -11,8 +10,12 @@ function RouteComponent() {
     "https://thefincheranalyst.com/wp-content/uploads/2018/07/1995-seven-john-doe.jpg";
 
   return (
-    <section>
-      <Profile width="25" height="25" imageUrl={imageUrl} />
+    <section className="w-full flex flex-row gap-4 justify-start pl-2">
+      <Profile width="20" height="20" imageUrl={imageUrl} />
+      <div className="flex flex-col justify-center">
+        <h1 className="text-xl font-medium text-black">Account Username</h1>
+        <h3 className="font-medium text-slate-600">Account Owner</h3>
+      </div>
     </section>
   );
 }
