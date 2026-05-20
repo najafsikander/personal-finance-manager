3bd82fb42f59abc14da157e840bc9723ce2d72e9
najafsikander <najafsikander@yahoo.com>
2026-05-20 17:49:21 +0400

    Added TODOs in notification page

--- src/routes/settings/notifications/index.tsx
+++ src/routes/settings/notifications/index.tsx
@@ -1,9 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/settings/notifications/')({
+export const Route = createFileRoute("/settings/notifications/")({
   component: RouteComponent,
-})
-
+});
+//TODO: Implement notifications settings page
+//TODO: Add options for email notifications, push notifications, and in-app notifications
+//TODO: Allow users to customize notification preferences for different events (e.g., budget updates, bill reminders, etc.)
+//TODO: Implement a toggle for enabling/disabling notifications
 function RouteComponent() {
-  return <div>Hello "/settings/notifications/"!</div>
+  return <div>Hello "/settings/notifications/"!</div>;
 }
