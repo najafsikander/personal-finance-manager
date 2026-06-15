36ba68fca201d6bec43b2744b10134e3d56e9e8a
najafsikander <najafsikander@yahoo.com>
2026-06-15 11:23:50 +0400

    Added dropdown for notifications button to show list of notifications

--- src/components/custom/NotificationBtn.tsx
+++ src/components/custom/NotificationBtn.tsx
@@ -1,16 +1,43 @@
 import { Bell, BellDot } from "lucide-react";
-import { FC } from "react";
+import { FC, useState } from "react";
+import clsx from "clsx";
 
 const NotificationBtn: FC = () => {
   const [hasNotifications] = [true]; // Placeholder for notification state
+  const [toggleNotification, setToggleNotification] = useState(false); // Placeholder for notification state
+
   return (
     <>
       <button
         className="p-2 hover:bg-gray-700 rounded-lg transition-colors relative cursor-pointer"
         aria-label="Notifications"
+        onClick={() => setToggleNotification((prev) => !prev)}
       >
         {hasNotifications ? <BellDot size={30} /> : <Bell size={30} />}
       </button>
+      <div
+        className={clsx(
+          "absolute top-17 right-17 w-64 bg-white shadow-lg rounded-lg p-4 z-50",
+          { block: toggleNotification, hidden: !toggleNotification },
+        )}
+      >
+        <ul className="divide-y divide-gray-200">
+          <li className="border-b border-gray-200 py-2">
+            <p className="text-sm text-gray-700">New message from John Doe</p>
+            <span className="text-xs text-gray-500">5 minutes ago</span>
+          </li>
+          <li className="border-b border-gray-200 py-2">
+            <p className="text-sm text-gray-700">
+              Your report is ready to download
+            </p>
+            <span className="text-xs text-gray-500">10 minutes ago</span>
+          </li>
+          <li className="py-2">
+            <p className="text-sm text-gray-700">New comment on your post</p>
+            <span className="text-xs text-gray-500">15 minutes ago</span>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
