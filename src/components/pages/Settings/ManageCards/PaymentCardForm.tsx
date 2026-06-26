0a38e3d0bbe83b3862164ae16c2b08bac4943f66
najafsikander <najafsikander@yahoo.com>
2026-06-26 16:19:34 +0400

    Creating manage cards form

--- src/components/pages/Settings/ManageCards/PaymentCardForm.tsx
+++ src/components/pages/Settings/ManageCards/PaymentCardForm.tsx
@@ -1,4 +1,22 @@
+import { paymentCardDefault } from "@/data/default/payment-card";
+import { useAppForm } from "@/lib/forms/useAppForm";
+import { paymentCardSchema } from "@/schemas/payment-card";
+
 const PaymentCardForm = () => {
+  const form = useAppForm({
+    defaultValues: paymentCardDefault,
+    validators: {
+      onChange: paymentCardSchema,
+    },
+    onSubmit: ({ value }) => {
+      console.log("Value: ", form.state.values);
+      alert(JSON.stringify(value, null, 2));
+    },
+    onSubmitInvalid: ({ formApi }) => {
+      console.log("INVALID submit", formApi.state.errors);
+    },
+  });
+
   return <></>;
 };
 
